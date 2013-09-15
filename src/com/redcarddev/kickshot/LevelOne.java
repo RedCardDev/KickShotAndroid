4cdbaeaf9849ec1bf14b9b2da8d15d76b6be0cff
otternq <otternq@gmail.com>
2013-09-14 17:32:13 -0700

    Adding a view to display the game board and loading the home chip

---
+++ src/com/redcarddev/kickshot/LevelOne.java
@@ -0,0 +1,24 @@
+package com.redcarddev.kickshot;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+
+public class LevelOne extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.level_one);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		
+		return true;
+	}
+
+}
