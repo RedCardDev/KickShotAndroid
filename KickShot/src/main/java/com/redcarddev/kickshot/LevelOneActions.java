7a5e0ef0a4cc8300c7d462898a22cf42a7b30556
otternq <otternq@gmail.com>
2013-10-27 16:14:33 -0700

    #37 - starting to create action

---
+++ KickShot/src/main/java/com/redcarddev/kickshot/LevelOneActions.java
@@ -0,0 +1,25 @@
+package com.redcarddev.kickshot;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+/**
+ * Created by otternq on 10/27/13.
+ */
+public class LevelOneActions extends Activity {
+
+    String LOGTAG = this.getClass().getName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.level_one_actions);
+    }
+
+
+
+}
