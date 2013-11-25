3c322f75e18894983b765a3246df8f5bf49d73b8
Jordan Leithart <leit7193@vandals.uidaho.edu>
2013-11-25 13:54:37 -0800

    dev issue 39
    I got the images showing up when the actions happen. We're still missing
    a scoring image. I need to add a close image button and they often load
    over each other. Not sure how to fix that.

--- KickShot/src/main/java/com/redcarddev/kickshot/LevelOneActions.java
+++ KickShot/src/main/java/com/redcarddev/kickshot/LevelOneActions.java
@@ -1,11 +1,9 @@
 package com.redcarddev.kickshot;
 
+import java.util.Random;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -16,15 +14,22 @@ public class LevelOneActions extends Activity {
 
     String LOGTAG = this.getClass().getName();
 
+    static Random r = new Random();
+
     final static int COMPUTER_SCORED = 1;
     final static int COMPUTER_BLOCKED = 3;
     final static int COMPUTER_SHOT = 5;
+    final static int COMPUTER_INTERCEPT = 7;
+    final static int COMPUTER_TURNOVER = 9;
 
     final static int PLAYER_SCORED = 0;
     final static int PLAYER_BLOCKED = 2;
     final static int PLAYER_SHOT = 4;
+    final static int PLAYER_INTERCEPT = 6;
+    final static int PLAYER_TURNOVER = 8;
 
     protected int state = -1;
+    protected int whichSide;
 
     protected TextView actionText;
     protected ImageView actionImage;
@@ -37,6 +42,7 @@ public class LevelOneActions extends Activity {
 
         Intent mIntent = getIntent();
         this.state = mIntent.getIntExtra("state", -1);
+        this.whichSide = r.nextInt(100);
 
         this.actionText = (TextView)findViewById(R.id.actionText);
         this.actionImage = (ImageView)findViewById(R.id.actionImage);
@@ -48,26 +54,66 @@ public class LevelOneActions extends Activity {
     protected boolean setActionView() {
 
         String action = "";
-        String image = "";
-
+        // every time this is called randomize if it's a shot to the left or right
         switch (this.state) {
             case LevelOneActions.COMPUTER_SCORED:
                 action = "They scored!";
                 break;
             case LevelOneActions.COMPUTER_BLOCKED:
                 action = "They blocked your shot!";
+                if(this.whichSide >= 50){
+                    this.actionImage.setImageResource(R.drawable.away_block_left);
+                }
+                else{
+                    this.actionImage.setImageResource(R.drawable.away_block_right);
+                }
                 break;
             case LevelOneActions.COMPUTER_SHOT:
                 action = "They are shooting!";
+                if(this.whichSide >= 50){
+                    this.actionImage.setImageResource(R.drawable.away_shot_left);
+                }
+                else{
+                    this.actionImage.setImageResource(R.drawable.away_shot_left);
+                }
+                break;
+            case LevelOneActions.COMPUTER_INTERCEPT:
+                action = "They intercepted the ball!";
+                this.actionImage.setImageResource(R.drawable.away_intercept);
+                break;
+            case LevelOneActions.COMPUTER_TURNOVER:
+                action = "They turned over the ball!";
+                this.actionImage.setImageResource(R.drawable.home_intercept);
                 break;
             case LevelOneActions.PLAYER_SCORED:
                 action = "You scored!";
                 break;
             case LevelOneActions.PLAYER_BLOCKED:
                 action = "You blocked their shot!";
+                if(this.whichSide >= 50){
+                    this.actionImage.setImageResource(R.drawable.home_block_left);
+                }
+                else{
+                    this.actionImage.setImageResource(R.drawable.home_block_right);
+                }
                 break;
             case LevelOneActions.PLAYER_SHOT:
                 action = "You are shooting!";
+                this.actionImage.setImageResource(R.drawable.home_shot_left);
+                if(this.whichSide >= 50){
+                    this.actionImage.setImageResource(R.drawable.home_shot_left);
+                }
+                else{
+                    this.actionImage.setImageResource(R.drawable.home_shot_right);
+                }
+                break;
+            case LevelOneActions.PLAYER_INTERCEPT:
+                action = "You intercepted the ball!";
+                this.actionImage.setImageResource(R.drawable.home_intercept);
+                break;
+            case LevelOneActions.PLAYER_TURNOVER:
+                action = "You turned the ball over";
+                this.actionImage.setImageResource(R.drawable.away_intercept);
                 break;
             default:
                 return false;
